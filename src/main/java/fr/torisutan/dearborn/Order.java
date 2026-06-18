4e645b17344499f89e53b8b1c5f69dae033d1474
TorisutanKholwes <tristan.clowez@opensvc.com>
2026-06-18 14:36:26 -0400

    implement order management with observer pattern and pricing logic

--- src/main/java/fr/torisutan/dearborn/Order.java
+++ src/main/java/fr/torisutan/dearborn/Order.java
@@ -1,25 +1,69 @@
 package fr.torisutan.dearborn;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
 
-    public void attach(OrderObserver observer) {
+    private final int id;
 
+    private double price;
+    private double shippingPrice = 10.f;
+    private int count;
+
+    private final List<OrderObserver> observers = new ArrayList<>();
+
+    public Order(int id) {
+        this.id = id;
+    }
+
+    public void attach(OrderObserver observer) {
+        observers.add(observer);
     }
 
     public void detach(OrderObserver observer) {
+        observers.remove(observer);
+    }
 
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getShippingPrice() {
+        return shippingPrice;
+    }
+
+    public void setShippingPrice(double shippingPrice) {
+        this.shippingPrice = shippingPrice;
     }
 
     public double getTotalPrice() {
-        return 0.f;
+        return price + shippingPrice;
     }
 
     public int getCount() {
-        return 0;
+        return count;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void addItem(double price) {
+        this.price += price;
+        this.count++;
+        System.out.format("Add item with price $%.2f\n", price);
+        for (OrderObserver observer : observers) {
+            observer.update(this);
+        }
     }
 
     @Override
     public String toString() {
-        return "";
+        return String.format("Order %d has %d items and costs $%.2f. With shipping, it costs $%.2f.\n", id, count, price, getTotalPrice());
     }
 }
--- src/main/java/fr/torisutan/dearborn/QuantityObserver.java
+++ src/main/java/fr/torisutan/dearborn/QuantityObserver.java
@@ -3,5 +3,9 @@ package fr.torisutan.dearborn;
 public class QuantityObserver implements OrderObserver {
     @Override
     public void update(Order order) {
+        if (order.getCount() > 5) {
+            order.setShippingPrice(0);
+            System.out.println("Remove shipping price");
+        }
     }
 }
